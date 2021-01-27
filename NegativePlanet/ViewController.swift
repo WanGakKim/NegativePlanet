c1e08c6c7b43d9fdfb41dac3f8bc44833e63275a
wangak kim <kimgak22@gmail.com>
2021-01-27 22:07:08 +0900

    Initial Commit

---
+++ NegativePlanet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NegativePlanet
+//
+//  Created by Gak_ee on 2021/01/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
